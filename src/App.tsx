02d063dedf5a4ffc3c12c96e456d74edd35d55cc
samaila-bashir <developer.chatto@gmail.com>
2025-01-02 16:18:50 +0100

    configured ShadCN

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <div>
       <h1>React Typescript Template</h1>
+      <Button>ShadCN Ready</Button>
     </div>
   );
 }
