07b99c7590c11251839c39e8ce145acce6cbb09d
samaila-bashir <developer.chatto@gmail.com>
2025-01-03 22:51:58 +0100

    implemented skeleton for weather dashboard

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,7 +1,28 @@
+import WeatherSkeleton from "@/components/location-skeleton";
 import { Button } from "@/components/ui/button";
+import { useGeolocation } from "@/hooks/use-geolocation";
 import { RefreshCw } from "lucide-react";
 
 const WeatherDashboard = () => {
+  const {
+    coordinates,
+    error: locationError,
+    getLocation,
+    isLoading: locationLoading,
+  } = useGeolocation();
+
+  const handleRefresh = () => {
+    getLocation();
+
+    if (coordinates) {
+      // reload weather data
+    }
+  };
+
+  if (locationLoading) {
+    return <WeatherSkeleton />;
+  }
+
   return (
     <div className="space-y-4">
       <div className="flex items-center justify-between">
@@ -9,7 +30,7 @@ const WeatherDashboard = () => {
         <Button
           variant="outline"
           size="icon"
-          // onClick={handleRefresh}
+          onClick={handleRefresh}
           // disabled={}
         >
           <RefreshCw className="h-4 w-4" />
